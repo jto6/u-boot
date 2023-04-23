22cc9e7117a7f0cdf6bae5d6d3e7fc81ee32974a
Jon Humphreys <accounts.jon@gmail.com>
2023-04-23 12:23:39 -0500

    modifications to switch back to distro boot (from standard boot) and enable NVME as well as USB boot sequence.
    Signed-off-by: Jon Humphreys <accounts.jon@gmail.com>

--- include/configs/qemu-arm.h
+++ include/configs/qemu-arm.h
@@ -6,9 +6,37 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+#ifdef CONFIG_CMD_USB
+# define BOOT_TARGET_USB(func)	func(USB, usb, 0)
+#else
+# define BOOT_TARGET_USB(func)
+#endif
+
+#define BOOT_TARGET_DEVICES(func)               \
+  func(NVME, nvme, 0)                          \
+	BOOT_TARGET_USB(func)                         \
+    func(QEMU, qemu, na)                        \
+    func(VIRTIO, virtio, 0)                     \
+    func(SCSI, scsi, 0)                         \
+    func(DHCP, dhcp, na)
+
+#include <config_distro_bootcmd.h>
+
 /* Physical memory map */
 #define CFG_SYS_SDRAM_BASE		0x40000000
 
 /* For timer, QEMU emulates an ARMv7/ARMv8 architected timer */
+
+#define BOOTENV_DEV_QEMU(devtypeu, devtypel, instance)  \
+	"bootcmd_qemu="                                       \
+  "if env exists kernel_start; then "                   \
+  "bootm ${kernel_start} - ${fdtcontroladdr};"          \
+  "fi;\0"
+
+#define BOOTENV_DEV_NAME_QEMU(devtypeu, devtypel, instance) \
+	"qemu "
+
+#define CFG_EXTRA_ENV_SETTINGS                  \
+	BOOTENV
 
 #endif /* __CONFIG_H */
